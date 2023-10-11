e618d625432df29429a48f29644104eabaecd843
Ronald Mok <ronaldmok01@gmail.com>
2023-10-11 09:28:35 +0800

    Test Lesson 1

---
+++ C++/Chapter 1/intro.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+/* This is a multi-line comment, starting with */
+{
+    std::cout << "Hello World!\n"; // This statement prints out "Hello World"
+    return 0;
+}
--- C++/Chapter 2/hello_world.cpp
+++ C++/Chapter 2/hello_world.cpp
@@ -6,5 +6,6 @@ using namespace std;
 int main()
 {
     cout << "Hello, World!\n";
+    cout << "Here we go!\n";
     return 0;
 }
