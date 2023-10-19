dd546213b7d79ccced3db4e1bd449d576055863f
Ronald <ronaldmok01@gmail.com>
2023-10-19 14:34:58 +0800

    Learnt about literals and operators

--- C++/Chapter 1/intro.cpp
+++ C++/Chapter 1/intro.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(){
+
+    int x, y;                                                        // Create 2 variables x and y
     
-    return 0;
+    std::cout << "Enter 1st number: ";                               // Receives input for x
+    std::cin >> x;
+    std::cout << "Enter 2nd number: ";                               // Receives input for y       
+    std::cin >> y;
+
+    std::cout << "The sum of x and y is: " << x + y << '\n';         // Final Sum of x and y
+
+
 }
