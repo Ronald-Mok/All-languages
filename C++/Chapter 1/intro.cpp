91195a097407c84f23eb037642eb4e8654f02b9f
Ronald <ronaldmok01@gmail.com>
2023-10-19 11:01:18 +0800

    Made default run code c++20

--- C++/Chapter 1/intro.cpp
+++ C++/Chapter 1/intro.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main()
-/* This is a multi-line comment, starting with */
 {
-    std::cout << "Hello World!\n"; // This statement prints out "Hello World"
+    std::cout << "Hello World!\n";
+    
     return 0;
 }
