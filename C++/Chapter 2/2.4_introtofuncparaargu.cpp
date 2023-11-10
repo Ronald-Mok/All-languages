44e292ab9088df5db1f7e696fd0ccfcb99a1a84a
Ronald Mok <ronaldmok01@gmail.com>
2023-11-10 10:22:05 +0800

    Edited on 10/11/23 1020h

--- C++/Chapter 2/2.4_introtofuncparaargu.cpp
+++ C++/Chapter 2/2.4_introtofuncparaargu.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 
-int getNum(int x=2){
-
+int getNum(int x=0){
+    
+    std::cout << "Enter a number: ";
+    std::cin >> x;
+    
     return x;
 }
 
 void calculate(int x, int y){
-
+    
     std::cout << "The sum is: " << x + y << '\n';
 
 }
 
 
 int main(){
-    calculate(getNum(), getNum(7));
+    calculate(getNum(), getNum());
 
     return 0;
 }
